cb76ca13c844e904a39bc09a71914c18180e0991
Kristján Oddsson <kristjan@cloudflare.com>
2017-01-28 22:51:23 +0000

    Clean up car package a bit and remove express server code

--- packages/car/index.js
+++ packages/car/index.js
@@ -1,59 +1,47 @@
-import request from 'request'
 import $ from 'cheerio'
-import h from 'apis-helpers'
-import app from '../../server'
+import request from 'request'
 
-const lookupCar = plate => new Promise((resolve, reject) => {
-  // Encode carPlate so that Icelandic characters will work
-  const carPlate = encodeURIComponent(plate)
-  const url = `http://www.samgongustofa.is/umferd/okutaeki/okutaekjaskra/uppfletting?vq=${carPlate}`
+import {apisUserAgent} from '../utils'
 
-  request.get({
-    headers: { 'User-Agent': h.browser() },
-    url,
-  }, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      reject('www.samgongustofa.is refuses to respond or give back data')
-    }
+export default (plate) => {
+  return new Promise((resolve, reject) => {
+    // Encode carPlate so that Icelandic characters will work
+    const carPlate = encodeURIComponent(plate)
+    const url = `http://www.samgongustofa.is/umferd/okutaeki/okutaekjaskra/uppfletting?vq=${carPlate}`
 
-    const data = $(body)
-    const fields = []
+    request.get({
+      headers: { 'User-Agent': apisUserAgent },
+      url,
+    }, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        reject('www.samgongustofa.is refuses to respond or give back data')
+      }
 
-    data.find('.vehicleinfo ul li').each(function () {
-      const val = $(this).find('span').text()
-      fields.push(val)
-    })
+      const data = $(body)
+      const fields = []
 
-    if (fields.length > 0) {
-      resolve({
-        type: fields[0],
-        subType: fields[0].substring(fields[0].indexOf('-') + 2, fields[0].indexOf('(') - 1),
-        color: fields[0].substring(fields[0].indexOf('(') + 1, fields[0].indexOf(')')),
-        registryNumber: fields[1],
-        number: fields[2],
-        factoryNumber: fields[3],
-        registeredAt: fields[4],
-        pollution: fields[5],
-        weight: fields[6],
-        status: fields[7],
-        nextCheck: fields[8],
+      data.find('.vehicleinfo ul li').each(function () {
+        const val = $(this).find('span').text()
+        fields.push(val)
       })
-    } else {
-      reject(`No car found with the registry number ${plate}`)
-    }
+
+      if (fields.length > 0) {
+        resolve({
+          type: fields[0],
+          subType: fields[0].substring(fields[0].indexOf('-') + 2, fields[0].indexOf('(') - 1),
+          color: fields[0].substring(fields[0].indexOf('(') + 1, fields[0].indexOf(')')),
+          registryNumber: fields[1],
+          number: fields[2],
+          factoryNumber: fields[3],
+          registeredAt: fields[4],
+          pollution: fields[5],
+          weight: fields[6],
+          status: fields[7],
+          nextCheck: fields[8],
+        })
+      } else {
+        reject(`No car found with the registry number ${plate}`)
+      }
+    })
   })
-})
-
-app.get('/car', (req, res) => {
-  const carPlate = req.query.number || req.query.carPlate || ''
-
-  if (!carPlate) {
-    return res.status(431).json({ error: 'Please provide a valid carPlate to lookup' })
-  }
-
-  lookupCar(carPlate)
-    .then(car => res.cache().json({ results: [car] }))
-    .catch(error => res.status(500).json({ error }))
-})
-
-export default lookupCar
+}
