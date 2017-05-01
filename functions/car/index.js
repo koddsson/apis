bc2a6d9a0bf25902195563bad1d87c4d0c9d52b1
Kristján Oddsson <kristjan@cloudflare.com>
2017-05-01 23:17:41 +0100

    car: update endpoint for terraform infrastructure

--- functions/car/index.js
+++ functions/car/index.js
@@ -1,21 +1,9 @@
 const lookupCar = require('@apis/car')
 
 exports.handle = (event, context) => {
-  // TODO: Move to utils
-  const queryString = event.querystring
-  const queryObject = queryString
-    .slice(1, -1)
-    .split(',')
-    .map((pair) => pair.trim())
-    .reduce((acc, curr) => {
-      const keyValueSplit = curr.split('=')
-      acc[keyValueSplit[0]] = keyValueSplit[1]
-      return acc
-    }, {})
-
-  const carPlate = queryObject.plate
+  const carPlate = event.params.querystring.plate;
 
   lookupCar(carPlate)
     .then((car) => context.succeed({results: [car]}))
-    .catch((error) => context.fail(error))
+    .catch((error) => context.fail(error));
 }
